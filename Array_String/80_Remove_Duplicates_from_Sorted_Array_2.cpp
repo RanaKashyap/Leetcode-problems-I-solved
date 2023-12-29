1c170e9ba5f78384d4556b4376baf973215b5559
Kashyap Rana <112256298+RanaKashyap@users.noreply.github.com>
2023-12-29 10:24:20 -0500

    Update 80_Remove_Duplicates_from_Sorted_Array_2.cpp

--- Array_String/80_Remove_Duplicates_from_Sorted_Array_2.cpp
+++ Array_String/80_Remove_Duplicates_from_Sorted_Array_2.cpp
@@ -13,26 +13,39 @@ int main () {
   int j = 1;
   for(int i = 1; i < nums.size(); i++){
 
-      if (nums[i] == nums[i-1] && counter == 0)
-      {
-        counter++;
-        j++;
-      }
+    if (nums[i] == nums[j-1] && counter == 0)
+    {
+      nums[j] = nums[i];
+      counter++;
+      j++;
+    }
 
-      if (nums[i] != nums[i - 1])
-      {
-        nums[j] = nums[i];
-        j++;
-        counter = 0;
-      }
+    if (nums[i] != nums[i - 1])
+    {
+      nums[j] = nums[i];
+      counter = 0;
+      j++;
+    }
   }
+  // return j;
 
+  // online solution
+  // int i =0;
+  // for(auto ele : nums)
+  // {
+  //     if(i==0 || i==1 || nums[i-2] != ele)
+  //     {
+  //         nums[i] = ele;
+  //         i++;
+  //     }
+  // }
+  //  return i ;
 
 
   // Print nums array in console
-  for (int i = 0; i < nums.size(); i++)
+  for (int k = 0; k < j; k++)
   {
-    cout << nums[i];
+    cout << nums[k];
   }
   cout << endl;
   cout << j;
