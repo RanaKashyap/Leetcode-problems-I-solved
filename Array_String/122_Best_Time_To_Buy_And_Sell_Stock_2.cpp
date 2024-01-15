594e0ff3e46506ecb559fb567c8836ff630ec445
Kashyap Rana <kashyaprana202@gmail.com>
2024-01-15 08:43:12 -0500

    best time to buy sell file

---
+++ Array_String/122_Best_Time_To_Buy_And_Sell_Stock_2.cpp
@@ -0,0 +1,23 @@
+// 122. Best Time to Buy and Sell Stock II
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*  
+  You want to maximize your profit by choosing a single day to buy one stock and choosing a
+  different day in the future to sell that stock.
+  Return the maximum profit you can achieve from this transaction. 
+  If you cannot achieve any profit, return 0.
+*/
+
+int main () {
+  // vector<int> prices = {7,1,5,3,6,4};
+  // Output: 5
+  // Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+
+
+
+  cout << "HELLO" << endl;
+  return 0;
+}
