36955d903a445d178a5af9659db943b3774f932e
RanaKashyap <kashyaprana202@gmail.com>
2024-02-13 14:00:00 -0500

    Best Buy Stock 2

--- Array_String/122_Best_Time_To_Buy_And_Sell_Stock_2.cpp
+++ Array_String/122_Best_Time_To_Buy_And_Sell_Stock_2.cpp
@@ -4,20 +4,33 @@
 
 using namespace std;
 
-/*  
-  You want to maximize your profit by choosing a single day to buy one stock and choosing a
-  different day in the future to sell that stock.
-  Return the maximum profit you can achieve from this transaction. 
-  If you cannot achieve any profit, return 0.
+/* 
+  You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+  On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time.
+  However, you can buy it then immediately sell it on the same day.
+
+  Find and return the maximum profit you can achieve.
 */
 
 int main () {
-  // vector<int> prices = {7,1,5,3,6,4};
-  // Output: 5
-  // Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+  /*
+    vector<int> prices = {7,1,5,3,6,4};
+    Output: 7
+    Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+    Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+    Total profit is 4 + 3 = 7.
+  */
+ // Linear Search
+  vector<int> prices = {7,1,5,3,6,4};
+  int profit = 0;
+
+  for (int i = 0; i < prices.size() - 1 ; i++) {
+    if (prices[i] < prices[i+1]) {
+      profit += prices[i+1] - prices[i];
+    }
+  }
 
 
-
-  cout << "HELLO" << endl;
+  cout << profit<< endl;
   return 0;
 }
