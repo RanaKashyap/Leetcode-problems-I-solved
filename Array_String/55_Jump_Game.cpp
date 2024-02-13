863c424c588bd5b1cdd38b37c232df6b9281a877
Kashyap Rana <kashyaprana202@gmail.com>
2024-02-13 17:29:49 -0500

    jump game

--- Array_String/55_Jump_Game.cpp
+++ Array_String/55_Jump_Game.cpp
@@ -2,3 +2,23 @@
 #include <vector>
 
 using namespace std;
+
+int main () {
+  vector<int> nums = {1,2,3};
+  bool res = false;
+  int n = nums.size();
+  int reach = 0;
+  for (int i = 0; i < n; i++) {
+      if(i > reach || reach >= n-1) {
+          break;
+      }
+      reach = max(reach, i + nums[i]);
+  }
+  if (reach >= n-1){
+    res = true;
+  }
+
+  //this "return false" means definitely (i > reach) at any point
+  // res = false;
+  return 0;
+}
