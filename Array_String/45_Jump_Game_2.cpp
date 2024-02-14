8ffe8b396fc185181ac249881843f18923cec30d
RanaKashyap <kashyaprana202@gmail.com>
2024-02-14 09:19:46 -0500

    jump game greedy

---
+++ Array_String/45_Jump_Game_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main () {
+  vector<int> nums = {2,3,1,1,4};
+  int min_jump = 0;
+  int nums_size = nums.size();
+  for (size_t i = 0; i < nums_size; i++) {
+    
+  }
+  
+
+
+  cout << "min jumps: " << min_jump << endl;
+  return 0;
+}
--- Array_String/55_Jump_Game.cpp
+++ Array_String/55_Jump_Game.cpp
@@ -6,19 +6,32 @@ using namespace std;
 int main () {
   vector<int> nums = {1,2,3};
   bool res = false;
-  int n = nums.size();
-  int reach = 0;
-  for (int i = 0; i < n; i++) {
-      if(i > reach || reach >= n-1) {
-          break;
-      }
-      reach = max(reach, i + nums[i]);
+  int goal = nums.size()-1;
+  // int n = nums.size();
+  // int reach = 0;
+  // for (int i = 0; i < n; i++) {
+  //     cout << i << " " << reach << endl;
+  //     if(i > reach || reach >= n-1) {
+  //         break;
+  //     }
+  //     reach = max(reach, i + nums[i]);
+  // }
+  // if (reach >= n-1){
+  //   res = true;
+  // }
+
+  for (int i = nums.size()-2; i >= 0; i--) {
+    if (i + nums[i] >= goal) {
+      goal = i;
+    }
   }
-  if (reach >= n-1){
+  
+  if (goal == 0) {
     res = true;
   }
+  
 
-  //this "return false" means definitely (i > reach) at any point
-  // res = false;
+
+  cout << "res " << res << endl;
   return 0;
 }
