5a2960f1ba736c702444aca76dd16b8ca3120eef
RanaKashyap <kashyaprana202@gmail.com>
2024-02-16 11:54:25 -0500

    jump game 2

--- Array_String/45_Jump_Game_2.cpp
+++ Array_String/45_Jump_Game_2.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main () {
-  vector<int> nums = {2,3,1,1,4};
-  int min_jump = 0;
-  int nums_size = nums.size();
-  for (size_t i = 0; i < nums_size; i++) {
-    
-  }
+  // BFS APPROACH
+  vector<int> nums = {2,3,1,1,4,5,6};
+  // vector<int> nums = {1,2,3};
+  int res = 0;
+  int left = 0, right = 0;
+  int goal = nums.size()-1;
   
+  while (right < goal) {
+    int farthest = 0;
+    for (int i = left; i < right+1; i++) {
+      farthest = max(farthest, i + nums[i]);
+    }
+    
+    left = right + 1;
+    right = farthest;
+    res++;
+  }
 
-
-  cout << "min jumps: " << min_jump << endl;
+  cout << "jumps " << res << endl;
   return 0;
 }
